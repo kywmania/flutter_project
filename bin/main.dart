98256fd42c80f4881d20d6baaecc1e07afe53ff6
KKK <kywmania0908@gmail.com>
2025-03-14 12:30:00 +0900

    코드 정리

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:my_project/shopping_mall.dart';
+import '../lib/shopping_mall.dart';
 
 void main() {
   ShoppingMall mall = ShoppingMall();
@@ -25,27 +25,10 @@ void main() {
         mall.confirmExit();
         break;
       case 5:
-<<<<<<< HEAD
-        print('삭제한 장바구니를 불러옵니다 !'); //삭제했던 장바구니 목록을 불러옴옴
-        mall.totalPrice = mall.removedTotalPrice;
-        mall.buyCart = mall.removedCart;
-        break;
-      case 6: //장바구니 상품목록, 가격 초기화
-        if (mall.totalPrice == 0) {
-          print('이미 장바구니가 비어있습니다 !');
-        } else {
-          print('장바구니를 초기화합니다 !'); //장바구니를 초기화 하면서 초기화 된 값을 다른 리스트에 저장
-          mall.removedTotalPrice = mall.totalPrice;
-          mall.totalPrice = 0;
-          mall.removedCart.addAll(mall.buyCart);
-          mall.buyCart.clear();
-        }
-=======
         mall.restoreRemovedCart();
         break;
       case 6: //장바구니 상품목록, 가격 초기화
         mall.clearCart();
->>>>>>> ef76e43 (객체지향 방식으로 함수 분리)
         break;
       default: //잘못된 메뉴 선택 시 출력
         print('지원하지 않는 기능입니다 ! 다시 시도해 주세요 ..');
